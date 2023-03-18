aa6022c81c3885c678f955838eee87821dfbd034
alexandrakst <alexandra.kostenko@hotmail.com>
2023-03-18 18:06:24 -0400

    phonebook update

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Ul = styled.ul`
+  margin-top: 0;
+`;
+
+export const Li = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+export const P = styled.p`
+  margin-right: 20px;
+`;
+
+export const Button = styled.button`
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
